477904fc8bb2e478a6cb45ec7f6821f7a9ca5034
Rinaldi Andrian Rahmanda <rinaldi.rahmanda@shopee.com>
2023-10-30 15:02:53 +0800

    add indexwriter commit checkpoints

--- src/indexer/DBLPIndexer.java
+++ src/indexer/DBLPIndexer.java
@@ -56,6 +56,7 @@ public class DBLPIndexer {
             indexWriter.addDocument(document);
             indexCount++;
             if (indexCount == decile) {
+                indexWriter.commit();
                 long currentTime = System.nanoTime();
                 System.out.printf(
                         "[INDEX] Decile #%d index time: %f seconds, total elapsed time: %f seconds\n",
@@ -68,6 +69,7 @@ public class DBLPIndexer {
         }
 
         if (decileCount != 10) {
+            indexWriter.commit();
             long currentTime = System.nanoTime();
             System.out.printf(
                     "[INDEX] Decile #%d index time: %f seconds, total elapsed time: %f seconds\n",
